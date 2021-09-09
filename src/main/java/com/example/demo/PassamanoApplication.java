b669d346d3bbebf89b9ad0444d9df3fb3238472f
soolsool <87021484+soolsool@users.noreply.github.com>
2021-09-09 15:29:50 +0900

    first commit

---
+++ src/main/java/com/example/demo/PassamanoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassamanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassamanoApplication.class, args);
+	}
+
+}
